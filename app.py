c602aba5264a12617f5438415d9d8bdedda6cef4
Huzaifa Imran <51008597+huzaifa003@users.noreply.github.com>
2023-08-31 19:23:12 +0000

    Finished

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ import model
 model.start()
 app = Flask(__name__)
 
-@app.route("/", methods = ['POST'])
+@app.route("/", methods = ['GET','POST'])
 def get_predictions():
+    if request.method == 'GET':
+        return render_template("questionJson.html")
     query = request.form['query']
     return model.predict(query)
+
 
 @app.route("/app", methods =['GET', 'POST'])
 def frontend():
     if request.method == "GET":
-        return render_template("answer")
+        return render_template("question.html")
     if request.method == "POST":
         query = request.form['query']
         prediction =  model.predict(query)
-        return prediction
+        print(prediction['answers'])
+        print(prediction['documents'])
+        print(prediction['query'])
+        return render_template("answer.html",answers = prediction['answers'], documents = prediction['documents'], query = prediction['query'])
     
